a915142b1c2d6dc611eba33b699af3253a38d9b4
patrick <codeflowbr@gmail.com>
2025-09-09 19:27:42 -0300

    feature: Patrick Issler Dal Lago

--- src/fag/Principal.java
+++ src/fag/Principal.java
@@ -1,10 +1,17 @@
 package fag;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Informe seu nome e sua idade:");
+		String nome = scan.nextLine();
+		int idade = scan.nextInt();
+		
+		System.out.println("Seu nome é:"+ nome +", Sua idade é: "+ idade);
 	}
 
 }
